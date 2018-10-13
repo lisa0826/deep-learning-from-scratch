ab8b6327f120cfdea1d66bbd1108bad7e65f5c0f
lisa_qiu <lisa_qiu@qq.com>
2018-10-13 09:23:54 +0800

    遗漏引用两个公式

--- 3-4-3.py
+++ 3-4-3.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def identity_function(x):
+	return x
+
 def init_network():
 	network = {}
 	network['W1'] = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
